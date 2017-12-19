9cecd902105cb24602697bb3e9181c90a3c54265
logeshpaul <logeshpaul@gmail.com>
2017-12-19 21:29:53 +0530

    Added few more ui apple scripts

--- osx/ui.sh
+++ osx/ui.sh
@@ -5,3 +5,32 @@ sudo -v
 
 # Set sidebar icon size to small
 defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
+
+# Set highlight color to green
+defaults write NSGlobalDomain AppleHighlightColor -string "0.764700 0.976500 0.568600"
+
+
+###############################################################################
+# Screen                                                                      #
+###############################################################################
+
+# Hide all desktop icons because who need 'em'
+# defaults write com.apple.finder CreateDesktop -bool false
+
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
